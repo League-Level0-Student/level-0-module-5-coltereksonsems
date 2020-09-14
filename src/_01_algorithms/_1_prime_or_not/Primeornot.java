9e5ff7fab751d5d78e440a639316673a53a815b9
root <root@f5fe811094bc>
2020-09-14 20:28:57 +0000

    Primeornot

---
+++ src/_01_algorithms/_1_prime_or_not/Primeornot.java
@@ -0,0 +1,26 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Primeornot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String prime =JOptionPane.showInputDialog("Please enter a number");
+		int number=Integer.parseInt(prime);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
