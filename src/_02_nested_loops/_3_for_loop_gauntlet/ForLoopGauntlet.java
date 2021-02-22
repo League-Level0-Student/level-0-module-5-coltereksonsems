6cb889c697dbe748057c3fa1f6105a53a03c55dc
student <student@ip-172-31-63-245.us-west-2.compute.internal>
2021-02-22 13:31:59 -0800

    I did some stuff

--- src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGauntlet.java
@@ -7,10 +7,12 @@ public class ForLoopGauntlet {
 		// loopOne();
 		// loopTwo();
 		// loopThree();
-		//loopFour();
-		//loopFive();
-	  	  //loopSix();
-		//loopSeven();
+		// loopFour();
+		// loopFive();
+		// loopSix();
+		// loopSeven();
+		//loopEight();
+		  loopNine();
 	}
 
 	static void loopOne() {
@@ -33,7 +35,7 @@ public class ForLoopGauntlet {
 	}
 
 	static void loopFour() {
-		
+
 		for (int i = 1; i <= 100; i++) {
 
 			if (i % 2 == 1)
@@ -41,32 +43,48 @@ public class ForLoopGauntlet {
 
 		}
 	}
-static void loopFive() {
-		
+
+	static void loopFive() {
+
 		for (int i = 1; i <= 100; i++) {
 
 			if (i % 2 == 1)
 				System.out.println(i);
-}}
-static void loopSix() {
-	for (int i = 7; i < 784; i+=7) {
-		System.out.println(i);
+		}
 	}
-}
 
-static void loopSeven (){
-	int age = 0;
-	for (int i = 2008; i < 2021; i++) {
-		System.out.println("in "+(i)+" i was "+(age)+" years old."); 
-		age++;
+	static void loopSix() {
+		for (int i = 7; i < 784; i += 7) {
+			System.out.println(i);
+		}
 	}
+
+	static void loopSeven() {
+		int age = 0;
+		for (int i = 2008; i < 2021; i++) {
+			System.out.println("in " + (i) + " i was " + (age) + " years old.");
+			age++;
+		}
+	}
+
+	static void loopEight (){
+		 
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println(i +" "+ j);
+			}
+		}
+	}
+		static void loopNine (){
+			
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+				System.out.println(j+i);	
+				}
+			}
+			
+		}
+		
+	
+
 }
-
-
-
-
-}
-		
-		
-		
-		
