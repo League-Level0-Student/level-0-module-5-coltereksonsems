72e0187bacf35217d18171bc8a3be77b43cf661e
student <student@ip-172-31-62-22.us-west-2.compute.internal>
2021-03-29 13:30:20 -0700

    obediant/happy.

--- src/_03_method_writing/_1_obedient_robot/ObediantRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObediantRobot.java
@@ -1,42 +1,56 @@
 package _03_method_writing._1_obedient_robot;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObediantRobot {
-public static void main(String[] args) {
-	
+	public static void main(String[] args) {
 
-Robot bob = new Robot();
-bob.setSpeed(50);
-bob.penDown();
-drawTriangle(bob);
+		Robot bob = new Robot();
+		bob.setSpeed(50);
+		bob.penDown();
 
+		String color = JOptionPane.showInputDialog("What color do you want?");
+		if (color.equals("red")) {
+			bob.setPenColor(Color.red);
 
-}
-static void drawSquare(Robot bob) {
-	for (int i = 0; i < 4; i++) {
-		bob.move(300);
-		bob.turn(90);
 		}
-	
-	
-	
-	
-	
-}	
-	static void drawTriangle (Robot bob) {
+		if (color.equals("blue")) {
+			bob.setPenColor(Color.blue);
+		}
+		if (color.equals("yellow")) {
+			bob.setPenColor(Color.yellow);
+		}
+		String shape = JOptionPane.showInputDialog("What shape do you want?");
+		if (shape.equals("square")) {
+			drawSquare(bob);
+		}
+		if (shape.equals("triangle")) {
+			drawTriangle(bob);
+		}
+		
+		
+		
+		
+		
+	}
+
+	static void drawSquare(Robot bob) {
+		for (int i = 0; i < 4; i++) {
+			bob.move(300);
+			bob.turn(90);
+		}
+
+	}
+
+	static void drawTriangle(Robot bob) {
 		for (int i = 0; i < 3; i++) {
-		bob.move(300);
-		bob.turn(120);
+			bob.move(300);
+			bob.turn(120);
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,17 +5,17 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+String petSpecies = JOptionPane.showInputDialog("What kind of pet do you want?"); 
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to increase your pet's happiness?", "Task chooser", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Take pet out for a walk", "Make pet some food", "Play with pet" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
 
@@ -27,4 +27,11 @@ public class HappyPet {
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	
+	
+	
+	
+	
+	
+	
 }
