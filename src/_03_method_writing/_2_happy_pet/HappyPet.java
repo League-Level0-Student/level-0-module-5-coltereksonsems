65cda76201a5836ed6c483f362fddbc22ca8c333
student <student@ip-172-31-52-129.us-west-2.compute.internal>
2021-04-05 13:27:26 -0700

    Happypet.Justhappypet.

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -10,7 +10,7 @@ public class HappyPet {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 String petSpecies = JOptionPane.showInputDialog("What kind of pet do you want?"); 
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
+		while (true) {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
@@ -18,10 +18,22 @@ String petSpecies = JOptionPane.showInputDialog("What kind of pet do you want?")
 					new String[] { "Take pet out for a walk", "Make pet some food", "Play with pet" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
-			// 6. If you determine the happiness level is large enough, tell the
+if (task == 0) {
+	walk ();
+}
+if (task == 1) {
+	food();
+}
+if (task == 2) {
+	play();
+}
+// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+ if (happinessLevel == 6) {
+	 JOptionPane.showInputDialog("You love your pet!"); 
+	 break;
+}
+		}
 	}
 
 	// 4. Create methods to handle each of your user selections.
@@ -29,8 +41,18 @@ String petSpecies = JOptionPane.showInputDialog("What kind of pet do you want?")
 	//    and INCREMENT the pet's happiness Level.
 	
 	
-	
-	
+	static void walk() {
+		JOptionPane.showMessageDialog (null, "you took your pet for a walk");
+		happinessLevel +=1;
+	}
+	static void food() {
+		JOptionPane.showMessageDialog (null, "you fed your pet");
+		happinessLevel +=1;
+	}
+	static void play() {
+		JOptionPane.showMessageDialog (null, "you played with your pet");
+		happinessLevel +=1;
+	}
 	
 	
 	
